a2172592aedbc4e9ac683cf9bc7d5be6abcb237e
GPPets <gppetrova@gmail.com>
2015-06-21 22:58:03 +0300

    Homework 14 (Threads) added

--- GPPets_hw14/src/Problem_p49_1/Main.java
+++ GPPets_hw14/src/Problem_p49_1/Main.java
@@ -1,5 +1,10 @@
 package Problem_p49_1;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,9 +15,38 @@ public class Main {
 		// то нека тя се представи. Решете задачата без да използвате thread pool.
 		// Решете същата задача по втори начин като създадете thread pool със 7 нишки, но използвате само 5 от тях.
 
+		MyThread t1 = new MyThread(3, "Beginning Thread");
+		MyThread t2 = new MyThread(1);
+		MyThread t3 = new MyThread(2, "KeepOnGoing Thread");
+		MyThread t4 = new MyThread();
+		MyThread t5 = new MyThread(9, "Last Thread");
+		MyThread t6 = new MyThread(1, "Sixth Thread");
+		MyThread t7 = new MyThread(5, "Seventh Thread");
 		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
 		
+		Queue<MyThread> threadQueue = new LinkedList<MyThread>();
+		threadQueue.offer(t1);
+		threadQueue.offer(t2);
+		threadQueue.offer(t3);
+		threadQueue.offer(t4);
+		threadQueue.offer(t5);
+		threadQueue.offer(t6);
+		threadQueue.offer(t7);
 		
+		ExecutorService queExecutor = Executors.newFixedThreadPool(7);
+		
+		for(MyThread t : threadQueue)
+			queExecutor.execute(t);
+		queExecutor.shutdown();
+		while (!queExecutor.isTerminated()) {
+		}
+		System.out.println("\nFinished ALL threads");
+				
 	}
 
 }
